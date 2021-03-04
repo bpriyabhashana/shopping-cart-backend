13b042497a15190ea812e16e95febedf65aa7c3d
priyabhashana.arimac <priyabhashana@arimaclanka.com>
2021-03-04 16:06:11 +0530

    first commit

---
+++ spring-boot/turning-point/src/main/java/com/api/turningpoint/TurningPointApplication.java
@@ -0,0 +1,13 @@
+package com.api.turningpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurningPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurningPointApplication.class, args);
+	}
+
+}
